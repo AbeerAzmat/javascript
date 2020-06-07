183203aacc941481f2de20d48256ccb0d10228d9
AbeerAzmat <57308368+AbeerAzmat@users.noreply.github.com>
2020-06-07 17:56:10 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -761,3 +761,235 @@ while (i <= strings.length){
 // var minutes = dt.getMinutes() ;
 // var finalTime = "Time  - " + hours + ":" + minutes + " " + AmOrPm; 
 // alert(finalTime);// final time 
+
+
+//Chapter 14-16
+//Task 1
+var studentName = [];
+
+//Task 3
+var stringArray = ['mustafa', 'ali', 'Pakistan', 'Karachi']
+
+//Task 4
+var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//Task 5
+var booleanArray = [true, false, false, true]
+
+//Task 6
+var mixedArray = ['myname ', 'pakisan', 'mustafa', 1, 5, 0, 8, 9, true, false]
+
+//Task 7
+var educationArray = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil.', 'PhD']
+for (var i = 0; i < educationArray.length; i++) {
+    document.write(i + "  " + educationArray[i] + "<br>")
+}
+
+//Task 8
+var studentName = ['hamza', 'salman', 'arsalan']
+var studentScore = [320, 230, 480];
+
+var percentagestudent1 = studentScore[0] * 100 / 500;
+var percentagestudent2 = studentScore[1] * 100 / 500;
+var percentagestudent3 = studentScore[2] * 100 / 500;
+var percentagestudents = [percentagestudent1, percentagestudent2, percentagestudent3]
+
+for (let index = 0; index < studentName.length; index++) {
+    document.write(`Score of ${studentName[index]} is ${studentScore[index]}.
+Percentage : ${percentagestudents[index]} %<br>`)
+}
+
+//Task 9 
+var colorName = ['red', 'green', 'blue', 'white', 'orange'];
+for (let index = 0; index < colorName.length; index++) {
+    document.write(colorName[index] + "<br>")
+}
+
+var userColor = prompt("Enter your Favorite Color");
+
+if (userColor != '' && userColor != undefined) {
+    // colorName.splice(0, 0, userColor)
+    colorName.unshift(userColor)
+    document.write(colorName + "</br>");
+
+}
+
+var userColor2 = prompt("Enter your Second Favorite Color");
+
+if (userColor2 != '' && userColor2 != undefined) {
+    // colorName.splice(0, 0, userColor)
+    colorName.push(userColor2)
+    document.write(colorName + "</br>");
+
+}
+
+colorName.unshift("pink")
+colorName.unshift("skyblue")
+document.write("<h3>New Updated Array: " + colorName + "</h3><br />")
+
+colorName.shift()
+document.write("<h3> First item removed and now New Updated Array: " + colorName + "</h3><br />")
+
+colorName.pop()
+document.write("<h3> Last item removed and now New Updated Array: " + colorName + "</h3><br />")
+
+var userColorName = prompt("Enter your color name")
+var userColorposition = parseInt(prompt("Enter which index you want to add color name"))
+colorName.splice(userColorposition, 0, userColorName)
+document.write("<h3>" + colorName + "</h3>")
+
+var userdeleteColor = prompt("Enter which index you want to delete color ")
+var userColorposition = parseInt(prompt("Enter how many colors you want to delete"))
+colorName.splice(userdeleteColor, userColorposition)
+document.write("<h3>" + colorName + "</h3><br>")
+
+
+//Task 10
+var sortArray = [120, 350, 220, 450, 100, 650, 300];
+document.write("Score of Students " + sortArray + "<br />")
+document.write(" Ordered Score of Students  " + sortArray.sort())
+
+//Task 11
+var cities = ['karachi', 'lahore', 'quetta', 'islamabad', 'Multan', 'peshawar']
+var selectedcities = cities.slice(2, 5);
+document.write("<br>Cities List  " + cities + "<br />")
+document.write("Cities List " + selectedcities + "<br />")
+
+//Task 12
+var arr = ['This', 'is', 'my', 'cat'];
+var stringArray = arr.join(" ")
+document.write(arr)
+document.write(stringArray)
+
+//Task 13
+var fifoarray = [];
+fifoarray.push('keyboard')
+fifoarray.push('mouse')
+fifoarray.push('printer')
+fifoarray.push('monitor')
+document.write("<h3>DEVICES: <br /> " + fifoarray + " </h3>")
+
+var out1 = fifoarray.shift()
+var out2 = fifoarray.shift()
+var out3 = fifoarray.shift()
+var out4 = fifoarray.shift()
+document.write("<h3>Out: <br /> " + out1 + " </h3>")
+document.write("<h3>Out: <br /> " + out2 + " </h3>")
+document.write("<h3>Out: <br /> " + out3 + " </h3>")
+document.write("<h3>Out: <br /> " + out4 + " </h3>")
+
+//Task 14
+var lifoArray = [];
+lifoArray.unshift('monitor')
+lifoArray.unshift('printer')
+lifoArray.unshift('mouse')
+lifoArray.unshift('keyboard')
+
+document.write("<h3>DEVICES: <br /> " + lifoArray + " </h3>")
+
+var out1 = lifoArray.pop()
+var out2 = lifoArray.pop()
+var out3 = lifoArray.pop()
+var out4 = lifoArray.pop()
+document.write("<h3>Out: <br /> " + out1 + " </h3>")
+document.write("<h3>Out: <br /> " + out2 + " </h3>")
+document.write("<h3>Out: <br /> " + out3 + " </h3>")
+document.write("<h3>Out: <br /> " + out4 + " </h3>")
+
+//Task 15
+var mobilesCompanies = ['Apple', 'Samsung', 'Motorola', 'Nokia', 'Sony', 'Haier']
+document.write(`<select><option>${mobilesCompanies[0]}</option><option>${mobilesCompanies[1]}</option><option>${mobilesCompanies[2]}</option><option>${mobilesCompanies[3]}</option><option>${mobilesCompanies[4]}</option><option>${mobilesCompanies[5]}</option></select>`)
+
+
+
+// chapter 17
+// 1. Declare and initialize an empty multidimensional array. (Array of arrays)
+var arr = new Array(3);
+for (var i = 0; i < arr.length; ++i) {
+  arr[i] = new Array(3);
+}
+
+console.log(arr);
+
+
+// 2. Declare and initialize a multidimensional array representing the  matrix:
+
+var num1 =[0,1,2,3];
+var num2 =[1,0,1,2];
+var num3 =[2,1,0,1];
+
+var num = [num1,num2,num3];
+console.log(num);
+
+// 3. Write a program to print numeric counting from 1 to 10.
+var x;
+for (x = 1;x <= 10; x++){
+  console.log(x)
+}
+
+
+// 4.  Write a program to print multiplication table of any number using for loop. Table number & length should be taken as an input from user.
+ var number=prompt("Enter a number to show its multipliclation table","Your number")
+ var number1=prompt("Enter length of multiplication table")
+ for (var i = 1; i <=number1; i++){
+  console.log("number" + "x" + i + "=" + number*i + " ")
+}
+
+
+// 5. Write a program to print items of the following array using for loop: fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]
+
+var fruits=["apple","banana","mango","orange","strawberry"];
+for (var i = 0; i <= 0; i++){
+  console.log(fruits)
+}
+
+// 6th task
+// counting
+for (var i = 1; i <=15; i++) {
+console.log(i)
+}
+
+// reverse counting
+
+for (var i = 10; i >= 1; i--) {
+ console.log(i)
+}
+
+// even counting
+for (var i = 0; i <=20; i=i+2) {
+  console.log(i)
+}
+
+// odd counting
+for (var i = 1; i < 20; i=i+2) {
+  console.log(i)
+}
+
+
+// var items=["cake","apple pie","cookie","chips","patties"];
+// var useritem =prompt("Enter your item")
+
+// var largestNumber = [24,53,78,91,12];
+// for(var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
+//   if(arr[arrayIndex] > largestNumber[arrayIndex]) {
+//
+//          largestNumber[arrayIndex] = arr[arrayIndex];
+//          return largestNumber
+// }
+
+
+// 10
+var sum = 0;
+function multiplesOf5(number) {
+
+
+  for(var i = 1; i < number; i++){
+    // if((i % 3 === 0 )
+    (i % 5 === 0)
+      sum = sum + i;
+
+  }
+  return sum;
+}
+
+multiplesOf5(100);
